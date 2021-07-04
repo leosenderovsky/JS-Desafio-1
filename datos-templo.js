7d7423425926418efd8a3b3efcd4d04ce6cb1c06
Leo Aquiba Senderovsky <l.a.senderovsky@gmail.com>
2021-07-04 09:20:04 -0300

    creacion index y archivo js conectado

---
+++ datos-templo.js
@@ -0,0 +1,11 @@
+alert('Ingrese aquí para inscribir la ceremonia que desea realizar en nuestro templo');
+
+let nombreApellido = prompt('Ingrese su nombre y apellido');
+let ceremonia = prompt('Ingrese la ceremonia que desea realizar');
+let nombreHomenajeado = prompt ('Ingrese el nombre de el/los homenajeados');
+let diaMes = prompt('Ingrese el día y mes en que desea realizar la ceremonia');
+let anio = prompt('Ingrese el año en que desea realizar la ceremonia');
+
+
+console.log(nombreApellido + ', estás inscribiéndote para realizar el ' + ceremonia + ' de ' + nombreHomenajeado + ' en nuestro templo el ' + diaMes + ' de ' + anio);
+alert(nombreApellido + ', estás inscribiéndote para realizar el ' + ceremonia + ' de ' + nombreHomenajeado + ' en nuestro templo el ' + diaMes + ' de ' + anio);
